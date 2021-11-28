2a20bdfb68cd9608721fe8e29300c71ff8d4079f
shaurya561 <77106149+shaurya561@users.noreply.github.com>
2021-11-28 16:30:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+song = "";
+song1 = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload() {
+    song=loadSound("Wolves.mp3");
+    song1=loadSound("masked_wolf.mp3");
+}
+
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center()
+    
+    video = createCapture(VIDEO);
+    video.hide();
+   
+    poseNet =ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+   }
+   
+   function modelLoaded() {
+       console.log('poseNet is initialized');
+   }
+   function gotPoses() {
+       if(results.length >0) {
+           console.log(results)
+           leftWristX = results[0].pose.leftWrist.x;
+           leftWristY = results[0].pose.leftWrist.y;
+           console.log("Left Wrist X :" + leftWristX + "Left Wrist y" + leftWristY);
+   
+           rightWristX = results[0].pose.rightWrist.x;
+           rightWristY = results[0].pose.rightWrist.y;
+           console.log("Right Wrist X :" + rightWristX + "Right Wrist y" + rightWristY);
+       }
+   }
+   
+   function draw() {
+       image(video,0,0,600,500);
+   }
+   
+   function play() {
+       song.play();
+       song.setVolume(1);
+       song.rate(1);
+   }
+   
